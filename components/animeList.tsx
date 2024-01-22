deb3968cbb7b8708533a49c76ab61655e9eb3e59
boxfish <boxfish893@gmail.com>
2024-01-22 23:40:38 +0900

    fix: buildエラーを修正

--- components/animeList.tsx
+++ components/animeList.tsx
@@ -14,26 +14,28 @@ const AnimeLists = ({
   ...props
 }: {
   animes: { title: string; id: string; viewers: number }[];
-} & AnchorHTMLAttributes<HTMLAnchorElement>) => {
+} & AnchorHTMLAttributes<HTMLElement>) => {
   return (
-    <ScrollArea {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle>公開中の話一覧</CardTitle>
-          <CardDescription>{`現在${animes.length}話まで公開されています。`}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          {animes.map((anime, id) => (
-            <Card key={id}>
-              <CardContent className="p-6 pb-0">{anime.title}</CardContent>
-              <CardHeader className="pt-3">
-                <CardDescription>{anime.viewers}再生</CardDescription>
-              </CardHeader>
-            </Card>
-          ))}
-        </CardContent>
-      </Card>
-    </ScrollArea>
+    <div {...props}>
+      <ScrollArea>
+        <Card>
+          <CardHeader>
+            <CardTitle>公開中の話一覧</CardTitle>
+            <CardDescription>{`現在${animes.length}話まで公開されています。`}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {animes.map((anime, id) => (
+              <Card key={id}>
+                <CardContent className="p-6 pb-0">{anime.title}</CardContent>
+                <CardHeader className="pt-3">
+                  <CardDescription>{anime.viewers}再生</CardDescription>
+                </CardHeader>
+              </Card>
+            ))}
+          </CardContent>
+        </Card>
+      </ScrollArea>
+    </div>
   );
 };
 
