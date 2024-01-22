8cf9fccdad7dcc2f87728fc5a1ccfeaa2a0e9f0e
boxfish <boxfish893@gmail.com>
2024-01-22 23:47:48 +0900

    fix: animeListの高さが揃わない問題を修正

--- components/animeList.tsx
+++ components/animeList.tsx
@@ -11,31 +11,30 @@ import { ScrollArea } from "./ui/scroll-area";
 
 const AnimeLists = ({
   animes,
-  ...props
+  className,
 }: {
   animes: { title: string; id: string; viewers: number }[];
-} & AnchorHTMLAttributes<HTMLElement>) => {
+  className: string;
+}) => {
   return (
-    <div {...props}>
-      <ScrollArea>
-        <Card>
-          <CardHeader>
-            <CardTitle>公開中の話一覧</CardTitle>
-            <CardDescription>{`現在${animes.length}話まで公開されています。`}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            {animes.map((anime, id) => (
-              <Card key={id}>
-                <CardContent className="p-6 pb-0">{anime.title}</CardContent>
-                <CardHeader className="pt-3">
-                  <CardDescription>{anime.viewers}再生</CardDescription>
-                </CardHeader>
-              </Card>
-            ))}
-          </CardContent>
-        </Card>
-      </ScrollArea>
-    </div>
+    <ScrollArea className={className}>
+      <Card>
+        <CardHeader>
+          <CardTitle>公開中の話一覧</CardTitle>
+          <CardDescription>{`現在${animes.length}話まで公開されています。`}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {animes.map((anime, id) => (
+            <Card key={id}>
+              <CardContent className="p-6 pb-0">{anime.title}</CardContent>
+              <CardHeader className="pt-3">
+                <CardDescription>{anime.viewers}再生</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
+        </CardContent>
+      </Card>
+    </ScrollArea>
   );
 };
 
