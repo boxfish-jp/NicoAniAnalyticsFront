808ddf4d1fd12159ed376e93090ac0aaf342e8d8
boxfish <boxfish893@gmail.com>
2024-03-10 03:33:29 +0900

    refactor: 不要なファイルやパッケージの削除

--- lib/queryUpTime.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./firebase";
-
-const queryUpTime = async () => {
-  const getUpTime = await db.collection("dbConfig").doc("LastFetch").get();
-  const upTime = Number(getUpTime.data()?.data);
-  return upTime;
-};
-export default queryUpTime;
