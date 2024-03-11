d7d064ea3eb65aead332b293aa2b857905167a8c
boxfish <boxfish893@gmail.com>
2024-03-11 22:53:17 +0900

    feat: 最新のviewDataが取れない問題が何故か解決 / #18

--- lib/queryVidViewData.ts
+++ lib/queryVidViewData.ts
@@ -2,7 +2,7 @@ import { dbEndpoint } from "./dbEndpoint";
 
 const queryVidViewData = async (ch_seq_id: number) => {
   const getVidViewDataUrl = new URL(dbEndpoint + "/viewData");
-  const params = { ch_seq_id: String(ch_seq_id) };
+  const params = { ch_seq_id: String(ch_seq_id), viewData: "true" };
   getVidViewDataUrl.search = new URLSearchParams(params).toString();
   const res = await fetch(getVidViewDataUrl.toString());
   const data = (await res.json()) as {
